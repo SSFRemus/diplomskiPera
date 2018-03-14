b4a77a046eb52d9edcaedb1513d83b41ac4e77c5
Petar <ssf_remus@yahoo.com>
2018-03-14 20:44:11 +0100

    splash screen and public reservations activity

---
+++ app/src/main/java/com/vidovic/petar/diplomski/PublicReservationsActivity.java
@@ -0,0 +1,51 @@
+package com.vidovic.petar.diplomski;
+
+import android.graphics.RectF;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class PublicReservationsActivity extends AppCompatActivity implements WeekView.EventClickListener, MonthLoader.MonthChangeListener, WeekView.EventLongPressListener, WeekView.EmptyViewLongPressListener {
+
+    private WeekView weekView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_public_reservations);
+
+        weekView = findViewById(R.id.weekView);
+
+        weekView.setOnEventClickListener(this);
+        weekView.setMonthChangeListener(this);
+        weekView.setEventLongPressListener(this);
+        weekView.setEmptyViewLongPressListener(this);
+    }
+
+    @Override
+    public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+        return null;
+    }
+
+    @Override
+    public void onEmptyViewLongPress(Calendar time) {
+    }
+
+    @Override
+    public void onEventClick(WeekViewEvent event, RectF eventRect) {
+    }
+
+    @Override
+    public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
+    }
+
+    public WeekView getWeekView() {
+        return weekView;
+    }
+}
