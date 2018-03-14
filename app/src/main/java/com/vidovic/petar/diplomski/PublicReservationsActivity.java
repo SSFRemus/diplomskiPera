e5a7557ed0c21a121c162d9e04694282a8d604ed
Petar <ssf_remus@yahoo.com>
2018-03-14 22:10:04 +0100

    android week view added

--- app/src/main/java/com/vidovic/petar/diplomski/PublicReservationsActivity.java
+++ app/src/main/java/com/vidovic/petar/diplomski/PublicReservationsActivity.java
@@ -8,12 +8,13 @@ import com.alamkanak.weekview.MonthLoader;
 import com.alamkanak.weekview.WeekView;
 import com.alamkanak.weekview.WeekViewEvent;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 public class PublicReservationsActivity extends AppCompatActivity implements WeekView.EventClickListener, MonthLoader.MonthChangeListener, WeekView.EventLongPressListener, WeekView.EmptyViewLongPressListener {
 
-    private WeekView weekView;
+    public WeekView weekView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +31,9 @@ public class PublicReservationsActivity extends AppCompatActivity implements Wee
 
     @Override
     public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-        return null;
+        List<WeekViewEvent> events = new ArrayList<>();
+
+        return events;
     }
 
     @Override
@@ -43,9 +46,5 @@ public class PublicReservationsActivity extends AppCompatActivity implements Wee
 
     @Override
     public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
-    }
-
-    public WeekView getWeekView() {
-        return weekView;
     }
 }
