6a2233dc21b55e2466b1ece85457095515b27497
Petar <ssf_remus@yahoo.com>
2018-04-01 18:11:53 +0200

    login now checks users database

--- app/src/main/java/com/vidovic/petar/diplomski/activity/LoginActivity.java
+++ app/src/main/java/com/vidovic/petar/diplomski/activity/LoginActivity.java
@@ -3,12 +3,21 @@ package com.vidovic.petar.diplomski.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.vidovic.petar.diplomski.R;
+import com.vidovic.petar.diplomski.manager.DatabaseManager;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,7 +26,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button loginButton = this.findViewById(R.id.loginButton);
+        final Button loginButton = findViewById(R.id.loginButton);
+        final EditText usernameEditText = findViewById(R.id.usernameEditText);
+        final EditText passwordEditText = findViewById(R.id.passwordEditText);
 
         findViewById(R.id.login_layout).setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -28,11 +39,70 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        usernameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().isEmpty()) {
+                    loginButton.setEnabled(false);
+                } else {
+                    if (!passwordEditText.getText().toString().isEmpty()) {
+                        loginButton.setEnabled(true);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        passwordEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().isEmpty()) {
+                    loginButton.setEnabled(false);
+                } else {
+                    if (!usernameEditText.getText().toString().isEmpty())
+                    loginButton.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, ReservationsActivity.class));
-                finish();
+                final String username = usernameEditText.getText().toString();
+                final String password = passwordEditText.getText().toString();
+
+                FirebaseDatabase.getInstance().getReference("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Boolean userFound = false;
+
+                        for (DataSnapshot user : dataSnapshot.getChildren()) {
+                            if (username.equals(user.getValue(String.class)) && password.equals(user.getValue(String.class))) {
+                                userFound = true;
+                                startActivity(new Intent(LoginActivity.this, ReservationsActivity.class));
+                                finish();
+                            }
+                        }
+
+                        if (!userFound) {
+                            Toast.makeText(getBaseContext(), "Korisničko ime ili šifra nisu ispravni.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {}
+                });
             }
         });
     }
