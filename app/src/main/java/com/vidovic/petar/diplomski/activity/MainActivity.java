58e92c485561575882307ae5edf27dcadda685e2
Petar <ssf_remus@yahoo.com>
2018-05-27 03:36:27 +0200

    ok http added, first steps towards the real application are being made

--- app/src/main/java/com/vidovic/petar/diplomski/activity/MainActivity.java
+++ app/src/main/java/com/vidovic/petar/diplomski/activity/MainActivity.java
@@ -24,11 +24,18 @@ import com.vidovic.petar.diplomski.R;
 import com.vidovic.petar.diplomski.manager.DatabaseManager;
 import com.vidovic.petar.diplomski.model.Event;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity implements MonthLoader.MonthChangeListener, WeekView.EventClickListener {
 
@@ -42,6 +49,26 @@ public class MainActivity extends AppCompatActivity implements MonthLoader.Month
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://rti.etf.bg.ac.rs/sale/apiView_bezBr.php?sala=26&mesec=5&godina=2018")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                } else {
+                    String str = response.body().string();
+                }
+            }
+        });
         setTitle(getResources().getString(R.string.overview));
 
         FirebaseDatabase.getInstance().getReference("events").addValueEventListener(new ValueEventListener() {
